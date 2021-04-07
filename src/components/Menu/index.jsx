9b879e005bfd8f6f9abe816d0922a18c34b9f8b6
Kelwy Oliveira <kelwyduarte@gmail.com>
2021-04-07 01:02:40 -0300

    Create toggle menu

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -1,6 +1,9 @@
-import React from 'react';
+/* eslint-disable prettier/prettier */
+/* eslint-disable object-curly-newline */
+import React, { useState } from 'react';
 import { FiSettings } from 'react-icons/fi';
-import { Container, Content, Settings } from './styles';
+import { MdMenu, MdClose } from 'react-icons/md';
+import { Container, Content, Settings, MenuToggle } from './styles';
 import logoImg from '../../assets/logo.png';
 import dashboardImg from '../../assets/dashboard.svg';
 import personagensImg from '../../assets/personagens.svg';
@@ -8,33 +11,49 @@ import planetasImg from '../../assets/planetas.svg';
 import naveImg from '../../assets/nave.svg';
 
 export function Menu() {
+  const [showToggleMenu, setshowToggleMenu] = useState(false);
+
+  const handleShowtoggleMenu = () => {
+    setshowToggleMenu(!showToggleMenu);
+    document.body.style.overflowY = showToggleMenu ? 'initial' : 'hidden';
+  };
+
   return (
-    <Container>
-      <Content>
-        <img src={logoImg} alt="Logo star wars" />
-        <ul>
-          <li>
-            <img src={dashboardImg} alt="Dashboard" />
-            <p>Dashboard</p>
-          </li>
-          <li>
-            <img src={personagensImg} alt="Personagens" />
-            <p>Personagens</p>
-          </li>
-          <li>
-            <img src={planetasImg} alt="Planetas" />
-            <p>Planetas e luas</p>
-          </li>
-          <li>
-            <img src={naveImg} alt="Naves" />
-            <p>Naves espaciais</p>
-          </li>
-        </ul>
-        <Settings>
-          <FiSettings size={20} />
-          <p>Configuração</p>
-        </Settings>
-      </Content>
-    </Container>
+    <>
+      <MenuToggle>
+        <button type="button" onClick={handleShowtoggleMenu}>
+          {showToggleMenu
+            ? <MdClose size="50" color="#fff" />
+            : <MdMenu size="50" color="#000" />}
+        </button>
+      </MenuToggle>
+      <Container className={showToggleMenu ? 'openMenu' : ''}>
+        <Content>
+          <img src={logoImg} alt="Logo star wars" />
+          <ul>
+            <li>
+              <img src={dashboardImg} alt="Dashboard" />
+              <p>Dashboard</p>
+            </li>
+            <li>
+              <img src={personagensImg} alt="Personagens" />
+              <p>Personagens</p>
+            </li>
+            <li>
+              <img src={planetasImg} alt="Planetas" />
+              <p>Planetas e luas</p>
+            </li>
+            <li>
+              <img src={naveImg} alt="Naves" />
+              <p>Naves espaciais</p>
+            </li>
+          </ul>
+          <Settings>
+            <FiSettings size={20} />
+            <p>Configuração</p>
+          </Settings>
+        </Content>
+      </Container>
+    </>
   );
 }
