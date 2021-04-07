a548103b48df306370bfa7eebb28a4c1b0db034d
Kelwy Oliveira <kelwyduarte@gmail.com>
2021-04-07 00:52:29 -0300

    Add responsive in chart

--- src/components/PieChart/styles.js
+++ src/components/PieChart/styles.js
@@ -7,4 +7,9 @@ export const ContainerChart = styled.div`
   background: var(--shape);
   box-shadow: 0px 4px 15px rgba(0, 0, 0, 0.25);
   border-radius: 1rem;
+
+  @media (max-width: 1214px) {
+    width: 660px;
+    justify-content: center;
+  }
 `;
